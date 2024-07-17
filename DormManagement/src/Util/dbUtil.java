75030b21dedb931ab337f1895d70bf33d3a460a6
naiqoac <cao00118@algonquinlive.com>
2024-07-16 22:32:21 -0400

    Update dbUtil.java

--- DormManagement/src/Util/dbUtil.java
+++ DormManagement/src/Util/dbUtil.java
@@ -7,8 +7,8 @@ import java.sql.*;
 public class dbUtil {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";  
     static final String DB_URL = "jdbc:mysql://localhost:3306/system_data?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-    static final String USER = "root";
-    static final String PASS = "&Million7";
+    static final String USER = "your user name";
+    static final String PASS = "your password";
     public static Connection getCon()throws Exception{
     	Class.forName(JDBC_DRIVER);
     	Connection con=DriverManager.getConnection(DB_URL,USER,PASS);
